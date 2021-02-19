4a3a4c2595a82d2c2ded0fd63863f615a3db44b7
Ivan Mladenov <ivan.mladenov@concordeu.com>
2021-02-19 14:44:37 +0200

    Create entity

---
+++ TodoApp.Domain/Common/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoApp.Domain.Common
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ TodoApp.Domain/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TodoApp.Domain.Common;
+
+namespace TodoApp.Domain
+{
+    public class Todo : Entity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
